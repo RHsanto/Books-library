9b3407773be38f367459d726e69ee9bbaabffd35
Rakibul santo <SANTODON176@gmail.com>
2021-09-02 18:53:43 +0600

    add some

--- scripts.js
+++ scripts.js
@@ -13,13 +13,15 @@ const searchBook = () =>{
   error.innerText ='';
   totalResults.innerText = '';
   bookContainer.textContent ='';
-  // e
+
+  // empty message
   if(searchText ===''){
-    error.innerHTML = ` <h3 class= "text-danger text-center" > This field is  Empety </h3>`;
+    error.innerHTML = ` <h3 class= "text-danger text-center" > This field is  Empty </h3>`;
     totalResults.innerText = '';
     bookContainer.textContent ='';
   }
 
+ // fetch the book url
 else{
   const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
@@ -28,30 +30,29 @@ else{
 }
 }
 
-
-
 // display book result and details  
 const displySearchResult = (books,totalBooks) => {
-  console.log(books,totalBooks)
 
+  // not fount message
    if(books.length === 0){
    error.innerHTML = ` <h3 class= "text-danger text-center" > Result not found </h3>`;
-   
   }
 
+  // total result here
   else{
 totalResults.innerHTML = `<p class =" text-center my-5 border p-5 w-50 mx-auto"> Showing <strong> ${books.length} </strong> results of <strong>${totalBooks} </strong>`
 }
 
-
-   const bookContainer = document.getElementById('book-container');
+// book container here
+ const bookContainer = document.getElementById('book-container');
        bookContainer.textContent ='';
-      
            books.forEach(book=>{
+
+             // creat a new div
                 const div = document.createElement('div');
                     div.classList.add('col');
                         div.innerHTML = `
-                         <div class="cards ">
+                         <div class="card ">
                         <img src= "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="Not Found">
                       <div class="card-body">
                      <h5 class="card-title">${book.title}</h5>
@@ -59,7 +60,7 @@ totalResults.innerHTML = `<p class =" text-center my-5 border p-5 w-50 mx-auto">
                   <p> <strong> Publisher </strong>  : ${book.publisher? book.publisher[0]:'Not Found'}  </p>
                   <p> First published in  <strong> ${book.first_publish_year? book.first_publish_year : 'Not Found' } </strong></p>
                      </div>
-                          </div> `
+                         </div> `
 
   
 bookContainer.appendChild(div)
